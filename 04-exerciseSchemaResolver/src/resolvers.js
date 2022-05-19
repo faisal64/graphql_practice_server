4068b7bc09097ab2825c73918a4e175cb8dd8685
faisal64 <faisalislam64@gmail.com>
2022-05-19 15:36:00 +0600

    add pics in 03 and add 04

--- 04-exerciseSchemaResolver/src/resolvers.js
+++ 04-exerciseSchemaResolver/src/resolvers.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   Query: {
-    pets(_, __, ctx){ //info -> all the fields that user queries will be in info. However, it is hardly used.
+    pets(_, __, ctx, info){ //info -> all the fields that user queries will be in info. However, it is hardly used.
       return ctx.models.Pet.findMany();
     }
   },
