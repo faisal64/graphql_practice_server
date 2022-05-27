801f00720f297d502b25e10d17698db07c40af7c
faisal64 <faisalislam64@gmail.com>
2022-05-27 15:42:29 +0600

    01 fixed a comment

--- 01-gqlSimpleServer/demo.js
+++ 01-gqlSimpleServer/demo.js
@@ -2,7 +2,7 @@
   /**
    * A spec that describes a declarative query language that your clients can use to ask an API for the exact 
    * data they want. This is achieved by creating a strongly typed Schema for your API, ultimately flexibility in
-   * how you API can resolve data, and client queries validated against your Schema.
+   * how your API can resolve data, and client queries validated against your Schema.
    */
 /////////////////////////////////////////////////////////////////////////////
 
